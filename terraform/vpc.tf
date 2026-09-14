1c1dd163695d36151460922d0729a8467c437dbe
holuwateemeeleyin <abegundeosamuel@gmail.com>
2026-09-14 13:56:51 +0100

    Add networking components

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,10 +1,169 @@
 resource "aws_vpc" "main" {
-    cidr_block            = "10.0.0.0/16"
-    enable_dns_support    = true
-    enable_dns_hostnames  = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-    tags = {
-        Name           = "three-tier-vpc"
-        Environment     = var.environment
-    }
+  tags = {
+    Name        = "three-tier-vpc"
+    Environment = var.environment
+  }
+}
+
+# Creating the public subnets
+
+resource "aws_subnet" "public" {
+  count = 2
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.${count.index + 1}.0/24"
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "public-subnet-${count.index + 1}"
+    Environment = var.environment
+    tier        = "public"
+  }
+}
+
+# Creating the private Web subnets
+resource "aws_subnet" "web" {
+  count = 2
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index + 11}.0/24"
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name        = "private-web-subnet-${count.index + 1}"
+    Environment = var.environment
+    tier        = "web"
+  }
+}
+
+# Creating the private App subnets
+resource "aws_subnet" "app" {
+  count = 2
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index + 21}.0/24"
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name        = "private-app-subnet-${count.index + 1}"
+    Environment = var.environment
+    tier        = "app"
+  }
+}
+
+# creating the database subnets
+resource "aws_subnet" "db" {
+  count = 2
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index + 31}.0/24"
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name        = "db-isolated-subnet-${count.index + 1}"
+    Environment = var.environment
+    tier        = "db"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "three-tier-igw"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name        = "public-route-table"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = 2
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name        = "nat-gateway-eip"
+    Environment = var.environment
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  depends_on = [
+    aws_internet_gateway.main
+  ]
+
+  tags = {
+    Name        = "three-tier-nat"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name        = "private-route-table"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table_association" "web" {
+  count = 2
+
+  subnet_id      = aws_subnet.web[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "app" {
+  count = 2
+
+  subnet_id      = aws_subnet.app[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "database-route-table"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route_table_association" "database" {
+  count = 2
+
+  subnet_id      = aws_subnet.db[count.index].id
+  route_table_id = aws_route_table.database.id
 }
